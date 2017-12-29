38040569aae5043627183973a06e7837372980ba
TheFizsz <Asianthunder1234@gmail.com>
2017-12-29 10:35:16 -0500

    Attributed files for the index
    These files handle the editing and animation within the index page.

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+ $(document).ready(function(){
+	
+      $("#circles").animate({opacity: '1'}, "slow");
+	  $(".circleList > *:nth-of-type(1)").css( { transition: "transform 0.5s",  /*Apply css transition animation through Jquery*/
+                              transform:  "translate(20em)" }, "slow" );
+							  
+	  $(".circleList > *:nth-of-type(2)").css( { transition: "transform 0.5s",
+                              transform:  "rotate(45deg) translate(20em) rotate(-45deg)" }, "slow" );
+							  
+		$(".circleList > *:nth-of-type(3)").css( { transition: "transform 0.5s",
+                              transform:  "rotate(90deg) translate(20em) rotate(-90deg)" }, "slow" );
+
+      $(".circleList > *:nth-of-type(4)").css( { transition: "transform 0.5s",
+                              transform:  "rotate(135deg) translate(20em) rotate(-135deg)" }, "slow" );	
+
+     $(".circleList > *:nth-of-type(5)").css( { transition: "transform 0.5s",
+                              transform:  " rotate(180deg) translate(20em) rotate(-180deg)" }, "slow" );
+
+      $(".circleList > *:nth-of-type(6)").css( { transition: "transform 0.5s",
+                              transform:  "rotate(225deg) translate(20em) rotate(-225deg)" }, "slow" );
+
+      $(".circleList > *:nth-of-type(7)").css( { transition: "transform 0.5s",
+                              transform:  "rotate(270deg) translate(20em) rotate(-270deg)" }, "slow" );	
+
+     $(".circleList > *:nth-of-type(8)").css( { transition: "transform 0.5s",
+                              transform:  "rotate(315deg) translate(20em) rotate(-315deg)" }, "slow" );							  
+ });
